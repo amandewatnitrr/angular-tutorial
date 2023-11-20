af9ab391902ca3c285af0bda0a35e645342d72cd
amandewatnitrr <amandewatnitrr@gmail.com>
2023-11-20 20:21:56 +0530

    Created Sample Angular Project

---
+++ typescript-basics/sample.js
@@ -0,0 +1,3 @@
+var counter = 10;
+var firstname = "Aman";
+console.log("".concat(firstname, " is ").concat(counter, " year old."));
---
+++ typescript-basics/sample.ts
@@ -0,0 +1,4 @@
+var counter: number = 10;
+let firstname: string = "Aman";
+
+console.log(`${firstname} is ${counter} year old.`);
